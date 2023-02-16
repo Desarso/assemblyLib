e21bc789733588023ab23919eecb157d5e03d1ae
desarso <malek.gabriel33@gmail.com>
2023-02-15 20:45:39 -0800

    all

--- fetchSelf/copyfetch.sh
+++ fetchSelf/copyfetch.sh
@@ -12,10 +12,19 @@ curl -LJO https://github.com/Desarso/assemblyLib/blob/main/Makefile
 curl -LJO https://github.com/Desarso/assemblyLib/blob/main/csc35.o
 curl -LJO https://github.com/Desarso/assemblyLib/blob/main/index.asm
 curl -LJO https://github.com/Desarso/assemblyLib/blob/main/script.sh
+curl -LJO https://github.com/Desarso/assemblyLib/blob/main/start.sh
+
+
 
 
 cp Makefile ./libs/Makefile
 cp csc35.o ./libs/csc35.o
 cp index.asm ./libs/index.asm
 cp script.sh ./libs/script.sh
+
+rm Makefile
+rm csc35.o
+rm index.asm
+rm script.sh
+rm fetch.sh
 
