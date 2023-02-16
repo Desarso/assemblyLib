cdeec6ccb3be1e74138d6493f653bb7b2b448db9
desarso <malek.gabriel33@gmail.com>
2023-02-15 21:09:13 -0800

    all

--- fetchSelf/fetch.sh
+++ fetchSelf/fetch.sh
@@ -8,14 +8,14 @@
 
 
 #this  fetches the files needed to set up lab
-curl -LJO https://github.com/Desarso/assemblyLib/blob/main/Makefile
-curl -LJO https://github.com/Desarso/assemblyLib/blob/main/csc35.o
-curl -LJO https://github.com/Desarso/assemblyLib/blob/main/index.asm
-curl -LJO https://github.com/Desarso/assemblyLib/blob/main/script.sh
-curl -LJO https://github.com/Desarso/assemblyLib/blob/main/start.sh
+curl -LJO https://raw.githubusercontent.com/Desarso/assemblyLib/main/Makefile
+curl -LJO https://raw.githubusercontent.com/Desarso/assemblyLib/main/csc35.o
+curl -LJO https://raw.githubusercontent.com/Desarso/assemblyLib/main/index.asm
+curl -LJO https://raw.githubusercontent.com/Desarso/assemblyLib/main/script.sh
+curl -LJO https://raw.githubusercontent.com/Desarso/assemblyLib/main/start.sh
 
 
-
+mkdir libs
 
 cp Makefile ./libs/Makefile
 cp csc35.o ./libs/csc35.o
@@ -26,5 +26,8 @@ rm Makefile
 rm csc35.o
 rm index.asm
 rm script.sh
+
+sh start.sh
+
 rm fetch.sh
 
